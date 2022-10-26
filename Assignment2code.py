2576b0e0946f0f9f11e8d525bde08d97b3b9970e
sajetha18 <115862279+sajetha18@users.noreply.github.com>
2022-10-26 09:54:56 +0530

    Delete Assignment2code.py

--- Assignment2code.py
+++
@@ -1,9 +0,0 @@
-import random
-while(True):
-    o=random.randint(10,99)
-    p=random.randint(10,99)
-    if(o > 30 and p > 75):
-     print("High Temprature and Humidity of:",o,p,"#","alarm is on")
-    elif(o < 35 and p < 65):
-         print("Normal Temprature and Humidity of:",o,p,"#","alarm is off")
-         break
